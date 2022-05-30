071f42b923854f3bf527dced06edbb97bfb45c26
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-05-30 19:55:24 +0200

    Cross Origin Resource Sharing

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template, url_for, session, redirect, jsonify
-
+from flask_cors import CORS
 import auth
 import data_manager
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = 'cd574346c04c28f66e13ba56001ff83c9987e3d96d0f4465'
 
 
